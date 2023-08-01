3f58d353a131c8758d768380910dbf4f30a88038
ShanKonduru <ShanKonduru@gmail.com>
2023-08-01 19:35:05 +0530

    help commands

--- pumbacontainer/createcontainer.bat
+++ pumbacontainer/createcontainer.bat
@@ -1,4 +1,4 @@
 @echo off
 REM run latest stable Pumba docker image (from master repository)
-docker run -itd -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba:master
+docker run -itd -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba:master ./pumba kill --interval 10s --signal SIGTERM
 
